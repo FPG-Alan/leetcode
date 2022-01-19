7cc505fe9875fd8b15a33b5b7a0d9c9dc290203f
alan <alan@fpi.cc>
2022-01-19 18:58:13 +0800

    u

---
+++ climb_stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  if (n === 2) {
+    return 2;
+  }
+
+  let res = [1, 2];
+  for (let i = 2; i < n; i++) {
+    res.push(res[i - 1] + res[i - 2]);
+  }
+
+  return res[res.length - 1];
+};
